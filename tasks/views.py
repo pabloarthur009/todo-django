0e4ebbb054044421fb24430533bf88a9ff7286e1
unknown <ferreira.pablo@escolar.ifrn.edu.br>
2025-08-10 18:13:19 -0300

    Implementa CRUD de tarefas

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Tarefa
+from .forms import TarefaForm
 
 def lista_tarefas(request):
     tarefas = Tarefa.objects.all()
@@ -8,3 +9,29 @@ def lista_tarefas(request):
 def detalhe_tarefa(request, pk):
     tarefa = get_object_or_404(Tarefa, pk=pk)
     return render(request, 'tasks/detalhe.html', {'tarefa': tarefa})
+
+def criar_tarefa(request):
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_tarefas')
+    else:
+        form = TarefaForm()
+    return render(request, 'tasks/form.html', {'form': form})
+
+def editar_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    if request.method == 'POST':
+        form = TarefaForm(request.POST, instance=tarefa)
+        if form.is_valid():
+            form.save()
+            return redirect('detalhe_tarefa', pk=pk)
+    else:
+        form = TarefaForm(instance=tarefa)
+    return render(request, 'tasks/form.html', {'form': form})
+
+def excluir_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    tarefa.delete()
+    return redirect('lista_tarefas')
