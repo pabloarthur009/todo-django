18d0fc5d932441f423aed37ee28fa229f6943d56
unknown <ferreira.pablo@escolar.ifrn.edu.br>
2025-08-10 18:05:57 -0300

    Implementa listagem e visualização de tarefas

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    concluida = models.BooleanField(default=False)
+    criada_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Tarefa
 
-# Create your views here.
+def lista_tarefas(request):
+    tarefas = Tarefa.objects.all()
+    return render(request, 'tasks/lista.html', {'tarefas': tarefas})
+
+def detalhe_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    return render(request, 'tasks/detalhe.html', {'tarefa': tarefa})
